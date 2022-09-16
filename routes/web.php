b1fcc6ba89925db647b6d3bbae2080cbbb3ab805
Bilal <bhaidar@gmail.com>
2022-09-16 11:14:40 +0300

    Add community controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,9 @@
 <?php
 
+use App\Http\Controllers\Backend\CommunityController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -24,8 +14,12 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::resource('/communities', CommunityController::class);
+});
 
 require __DIR__.'/auth.php';
