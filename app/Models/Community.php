d537e77f29f5eadcabb59c82953f71ea622b1031
Bilal <bhaidar@gmail.com>
2022-09-16 11:06:48 +0300

    Add model relationships and fillables

---
+++ app/Models/Community.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Community extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'slug',
+    ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'community_id',
+        'title',
+        'slug',
+        'description',
+        'url',
+        'votes',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function community()
+    {
+        return $this->belongsTo(Community::class);
+    }
+}
