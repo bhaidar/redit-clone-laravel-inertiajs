1c281781f31592035bf15fe204dc791c7e336971
Bilal <bhaidar@gmail.com>
2022-09-28 10:34:50 +0300

    Show create post only for authenticated users

--- resources/js/Pages/Frontend/Communities/Show.vue
+++ resources/js/Pages/Frontend/Communities/Show.vue
@@ -1,11 +1,16 @@
 <template>
     <GuestLayout>
+        <div class="flex justify-end m-2 p-2">
+            <NavLink v-if="$page.props.auth.auth_check" href="" class="">Create
+                Post</NavLink>
+        </div>
         <h1>Subreddit Show Page</h1>
         {{ community }}
     </GuestLayout>
 </template>
 
 <script setup>
+import NavLink from "@/Components/NavLink.vue";
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 
 defineProps({
