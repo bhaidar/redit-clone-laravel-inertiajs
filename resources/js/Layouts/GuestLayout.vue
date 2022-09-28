7ea2bdde76125eedb61a06d46bca444fdcbe50c4
Bilal <bhaidar@gmail.com>
2022-09-28 09:41:52 +0300

    use gueslayout for community.show

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -82,7 +82,7 @@ const showingNavigationDropdown = ref(false);
             </header>
 
             <!-- Page Content -->
-            <main>
+            <main class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 <slot />
             </main>
         </div>
--- resources/js/Pages/Frontend/Communities/Show.vue
+++ resources/js/Pages/Frontend/Communities/Show.vue
@@ -1,9 +1,13 @@
 <template>
-    <h1>Subreddit Show Page</h1>
-    {{ community }}
+    <GuestLayout>
+        <h1>Subreddit Show Page</h1>
+        {{ community }}
+    </GuestLayout>
 </template>
 
 <script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+
 defineProps({
     community: Object,
 });
